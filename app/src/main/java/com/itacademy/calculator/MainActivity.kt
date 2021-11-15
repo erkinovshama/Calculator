c4d7d5be85a63d0b6c155ab0393ba3b9044643bf
shamaerkinov <shama.erkinov@gmail.com>
2021-11-15 18:59:35 +0600

    Calculator

---
+++ app/src/main/java/com/itacademy/calculator/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.itacademy.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    lateinit var firstNumber: EditText
+    lateinit var symbol: EditText
+    lateinit var secondNumber: EditText
+
+
+    lateinit var button: Button
+
+    lateinit var result: TextView
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        firstNumber = findViewById(R.id.firstNumber)
+        symbol = findViewById(R.id.symbol)
+        secondNumber = findViewById(R.id.secondNumber)
+
+
+        button = findViewById(R.id.resultBtn)
+
+
+        result = findViewById(R.id.result)
+
+        button.setOnClickListener {
+            result.text = getResult()
+        }
+    }
+    fun getResult() : String {
+        val firstNumber = firstNumber.text.toString().toInt()
+        val secondNumber = secondNumber.text.toString().toInt()
+        val symbol = symbol.text.toString()
+
+
+        if (symbol == "+"){
+            return (firstNumber + secondNumber).toString()
+        }
+        if (symbol == "-"){
+            return (firstNumber - secondNumber).toString()
+        }
+        if (symbol == "*"){
+            return (firstNumber * secondNumber).toString()
+        }
+        if (symbol == "/"){
+            return (firstNumber / secondNumber).toString()
+        }
+        if (symbol == "="){
+            return (firstNumber == secondNumber).toString()
+        }
+        if (symbol == ">") {
+            return (firstNumber > secondNumber).toString()
+        }
+        if (symbol == "<") {
+            return (firstNumber < secondNumber).toString()
+        }
+        return "Нет результата"
+        }
+}
